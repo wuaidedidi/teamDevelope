f87df0852a396045d7740d1c67fad22e7b1a0eb2
wuaidedidi <470357714@qq.com>
2022-03-03 12:41:13 +0800

    sss

---
+++ src/lichen/com/develop.java
@@ -0,0 +1,15 @@
+package lichen.com;
+
+/**
+ * @author lichen
+ * @version 1.0.0
+ * @className develop.java
+ * @Description TODO
+ * @createTime 2022年03月03日 12:20:00
+ */
+public class develop {
+        public static void main (String[] args){
+                System.out.println(222);
+                System.out.println("提交本机代码");
+        }
+}
